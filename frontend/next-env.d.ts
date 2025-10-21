d897feb545a096283e14a8194689f736d5226a3e
Rehan Waris <rehan.waris@gmail.com>
2025-10-21 16:02:48 +0530

    Clean up next-env.d.ts file
    Removed type references and notes from next-env.d.ts

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,8 +1,3 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
 diff --git a/frontend/next-env.d.ts b/frontend/next-env.d.ts
 new file mode 100644
 index 0000000000000000000000000000000000000000..4f11a03dc6cc37f2b5105c08f2e7b24c603ab2f4
